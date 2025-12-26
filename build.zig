419445d0a6a339bb279e97a2d4a6c5c7ac4ab176
arthur.teixeira <arthurcarvalhot@yahoo.com.br>
2025-12-26 19:30:25 -0300

    Removed cat example

--- build.zig
+++ build.zig
@@ -4,17 +4,6 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const cat_exe = b.addExecutable(.{
-        .name = "cat",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/cat.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{},
-        }),
-    });
-    b.installArtifact(cat_exe);
-
     const zproxy_exe = b.addExecutable(.{
         .name = "zproxy",
         .root_module = b.createModule(.{
@@ -25,14 +14,6 @@ pub fn build(b: *std.Build) void {
         }),
     });
     b.installArtifact(zproxy_exe);
-
-    const run_cat_step = b.step("cat", "Run cat example");
-    const run_cat_cmd = b.addRunArtifact(cat_exe);
-    run_cat_step.dependOn(&run_cat_cmd.step);
-    run_cat_cmd.step.dependOn(b.getInstallStep());
-    if (b.args) |args| {
-        run_cat_cmd.addArgs(args);
-    }
 
     const run_zproxy_step = b.step("run", "Run zproxy example");
     const run_zproxy_cmd = b.addRunArtifact(zproxy_exe);
